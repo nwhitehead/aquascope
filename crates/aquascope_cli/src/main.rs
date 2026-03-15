2536e86b4a7564d45b9358b8fd4d7ad65334f7bd
Nathan Whitehead <nwhitehe@gmail.com>
2026-03-14 21:03:40 -0700

    Reformat properly.

--- crates/aquascope_cli/src/main.rs
+++ crates/aquascope_cli/src/main.rs
@@ -1,6 +1,6 @@
 use std::{
-  path::PathBuf,
-  process::{Command, Stdio},
+    path::PathBuf,
+    process::{Command, Stdio},
 };
 
 use anyhow::{Result, bail};
@@ -9,32 +9,31 @@ use log::{Level, debug, error, info, log_enabled};
 use tempfile::tempdir;
 
 fn main() -> Result<()> {
-  env_logger::init();
-  info!("Hello, world!");
-  let miri_sysroot = miri_sysroot()?;
-  let rustc = rustc()?;
-  let target_libdir_output = run_and_get_output(
-    Command::new(rustc.clone()).args(["--print", "target-libdir"]),
-  )?;
-  let target_libdir = PathBuf::from(target_libdir_output);
-  let tempdir = tempdir()?;
-  let root = tempdir.path();
+    env_logger::init();
+    info!("Hello, world!");
+    let miri_sysroot = miri_sysroot()?;
+    let rustc = rustc()?;
+    let target_libdir_output =
+        run_and_get_output(Command::new(rustc.clone()).args(["--print", "target-libdir"]))?;
+    let target_libdir = PathBuf::from(target_libdir_output);
+    let tempdir = tempdir()?;
+    let root = tempdir.path();
 
-  info!("SYSROOT={}", miri_sysroot.display());
-  info!("RUSTC={}", rustc.display());
-  info!("LD_LIBRARY_PATH={}", target_libdir.display());
-  info!("ROOT={}", root.display());
+    info!("SYSROOT={}", miri_sysroot.display());
+    info!("RUSTC={}", rustc.display());
+    info!("LD_LIBRARY_PATH={}", target_libdir.display());
+    info!("ROOT={}", root.display());
 
-  // use `cargo new` to create new empty rust project
-  let status = Command::new("cargo")
-    .args(["new", "--bin", "example"])
-    .current_dir(root)
-    .stdout(Stdio::null())
-    .stderr(Stdio::null())
-    .status()?;
-  if !status.success() {
-    bail!("Cargo failed");
-  }
+    // use `cargo new` to create new empty rust project
+    let status = Command::new("cargo")
+        .args(["new", "--bin", "example"])
+        .current_dir(root)
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .status()?;
+    if !status.success() {
+        bail!("Cargo failed");
+    }
 
-  Ok(())
+    Ok(())
 }
